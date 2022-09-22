3647e25bce9b7e4d6b9b419e9364d0d21215fe43
Sagar Gajare <56780589+Sagargajare@users.noreply.github.com>
2022-09-22 08:05:44 +0530

    home page ready

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -1,9 +1,57 @@
-import React from 'react'
-import { Container } from "@chakra-ui/react"
-const Home = () => {
-  return (
-    <Container>Home</Container>
-  )
-}
+import {
+  Flex,
+  Box,
+  Stack,
+  Button,
+  Text,
+  HStack,
+} from "@chakra-ui/react";
+import Image from "next/image";
 
-export default Home
+export default function SimpleCard() {
+  return (
+    <Flex
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={"#1B1B1B"}
+    >
+      
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6} align="center">
+        <HStack>
+          <Image width={"50"} height="50" src={"/img/logo.svg"} />
+          <Box>
+            <Text fontFamily={"Poppins"} fontWeight="700" fontSize={"24px"} color={"#ffffff"}>
+              Flaq
+            </Text>
+          </Box>
+        </HStack>
+        <Box
+          rounded={"md"}
+          bg={"#414141"}
+          boxShadow={"lg"}
+          p={8}
+        >
+          <Stack spacing={4}>
+            <Stack spacing={10} textAlign="center">
+              <Text fontFamily={"Poppins"} fontWeight="700" color={"#ffffff"} fontSize={"24px"}>
+                Welcome to the flaq academy
+              </Text>
+
+              <Button
+                bg={"#7C3AED"}
+                color={"white"}
+                _hover={{
+                  bg: "#9463e8",
+                }}
+                leftIcon={<Image width={"20"} height="20" src={"/img/discord-white.svg"} /> }
+              >
+                Connect with Discord
+              </Button>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
+}
--- theme/index.ts
+++ theme/index.ts
@@ -4,6 +4,10 @@ import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
 const config: ThemeConfig = {
   initialColorMode: "light",
   useSystemColorMode: false,
+  fonts: {
+    body: "Inter, sans-serif",
+    heading: `Inter, sans-serif`,
+  },
 };
 
 
