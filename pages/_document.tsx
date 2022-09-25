5c65a02a1ea0e5272b85b42e2e1330aae1039705
Sagar Gajare <56780589+Sagargajare@users.noreply.github.com>
2022-09-25 19:48:58 +0530

    login screen developed

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,9 +8,8 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
-            rel="stylesheet"
-          />
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+            rel="stylesheet"></link>
           <link
             href="http://fonts.cdnfonts.com/css/helvetica-neue-9"
             rel="stylesheet"
